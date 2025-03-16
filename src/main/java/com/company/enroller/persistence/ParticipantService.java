e3e57ac2849117a000a8987433bf9985a4d77b06
laszczak <anita.laszczak@gmail.com>
2025-03-16 12:15:22 +0100

    20250316 lab1

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -10,16 +11,39 @@ import com.company.enroller.model.Participant;
 @Component("participantService")
 public class ParticipantService {
 
-	DatabaseConnector connector;
+    DatabaseConnector connector;
 
-	public ParticipantService() {
-		connector = DatabaseConnector.getInstance();
-	}
+    public ParticipantService() {
+        connector = DatabaseConnector.getInstance();
+    }
 
-	public Collection<Participant> getAll() {
-		String hql = "FROM Participant";
-		Query query = connector.getSession().createQuery(hql);
-		return query.list();
-	}
+    public Collection<Participant> getAll() {
+        String hql = "FROM Participant";
+        Query query = connector.getSession().createQuery(hql);
+        return query.list();
+    }
+
+    public Participant findByLogin(String login) {
+        String hql = "FROM Participant WHERE login = :login";
+        Query query = connector.getSession().createQuery(hql);
+        query.setParameter("login", login);
+        Participant participant = (Participant) query.uniqueResult();
+        return participant;
+
+    }
+
+    public void saveParticipant(Participant participant) {
+
+        Transaction transaction = connector.getSession().beginTransaction();
+        connector.getSession().save(participant);
+        transaction.commit();
+
+    }
+
+    public void deleteParticipant(Participant participant) {
+        Transaction transaction = connector.getSession().beginTransaction();
+        connector.getSession().delete(participant);
+        transaction.commit();
+    }
 
 }
